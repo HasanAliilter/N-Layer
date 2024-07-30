99089f992020236efa24e44733318ddba45ae14f
Hasan Ali İlter <hasanilter003@gmail.com>
2024-07-30 13:10:58 +0300

    Entities have been created

---
+++ N-Layer.Core/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_Layer.Core.Models
+{
+    public class Team : BaseModel
+    {
+        public string Name { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
---
+++ N-Layer.Core/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_Layer.Core.Models
+{
+    public class User : BaseModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int TeamId { get; set; }
+        public Team Team { get; set; }
+        public UserProfile UserProfile { get; set; }
+    }
+}
